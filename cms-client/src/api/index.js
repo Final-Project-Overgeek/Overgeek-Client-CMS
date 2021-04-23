f463d5bd4915217ad3c493df4b4ebb1a7f270f53
brilianrn <brilian.rachmad13@gmail.com>
2021-04-23 13:14:38 +0700

    fix api 2

--- cms-client/src/api/index.js
+++ cms-client/src/api/index.js
@@ -1,4 +1,5 @@
 // const baseUrl = 'http://localhost:8080';
-const baseUrl = 'http://34.207.219.63:3000';
+// const baseUrl = 'http://34.207.219.63:3000';
+const baseUrl = 'http://54.242.82.197:3000';
 
 export default baseUrl;
